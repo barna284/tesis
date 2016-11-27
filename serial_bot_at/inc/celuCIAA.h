c3c1cd6c33031b60e19c47c034fd25b09912f62b
Ezequiel Martinez Vazquez <e.martinez.vazquez@gmail.com>
2016-11-27 14:24:28 -0300

    Commit inicial

---
+++ serial_bot_at/inc/celuCIAA.h
@@ -0,0 +1,9 @@
+#define ATsync "AT"
+#define ATOK "OK"
+#define ATOKreal "\r\nOK\r\n"
+#define ATcommand1 "+AT1"
+#define ATcommand2 "+AT2"
+#define ATcommand3 "+AT3"
+#define ATcommand4 "+AT4"
+
+int parse_AT (char const * bufcom, char[0][50]);
